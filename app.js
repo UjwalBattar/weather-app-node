3ec9b15be904e62178e8865539bd192b038d2945
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-22 18:27:15 -0700

    encode user input

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const config = require('./config.js');
 var myMapKey = config.config.MAP_KEY;
+
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+// console.log(argv);
+let encodedAddress = encodeURIComponent(argv.a);
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?key=${myMapKey}&address=1301%20lombard%20street%20philadelphia`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?key=${myMapKey}&address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
