f8378252aee7eb8731cb2f20ae38091a36f2413e
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-22 19:44:58 -0700

    wire up weather search with dark sky

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const config = require('./config.js');
+var myWeatherKey = config.config.DARK_SKY_KEY;
+
+const request = require('request');
+
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode.js');
 
 const argv = yargs
     .options({
-        a: {
+        lat: {
             demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
+            alias: 'latitude',
+            describe: 'Latitude to fetch weather for'
+        },
+        lng: {
+            demand: true,
+            alias: 'longitude',
+            describe: 'Longitude to fetch weather for'
         }
     })
     .help()
@@ -17,10 +26,46 @@ const argv = yargs
 
 // console.log(argv);
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage);
+
+request({
+    url: `https://api.darksky.net/forecast/${myWeatherKey}/${argv.lat},${argv.lng}`,
+    json: true
+}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        console.log('Temp: ', body.currently.temperature);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        console.log('Unable to fetch weather');
     }
+    // if (error) {
+    //     console.log('Unable to connect to Forecast.io servers');
+    // } else if (response.statusCode === 400) {
+    //     console.log('Unable to fetch weather');
+    // } else if (response.statusCode === 200) {
+    //     console.log('Temp: ', body.currently.temperature);
+    // }
 });
+
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
+
+// // console.log(argv);
+
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//         return;
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//         return results;
+//     }
+// });
